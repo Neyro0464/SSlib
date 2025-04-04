387f084c0632a205763d2e24918d506a24a02bee
Neyro0464 <zuber0464@gmail.com>
2025-04-04 20:59:11 +0700

    Update

--- Source.cpp
+++ Source.cpp
@@ -1,31 +1,27 @@
-#include <iostream>
-#include <string>
-#include "libSatSelect/SatSelect.h"
+#include<iostream>
+#include<string>
+#include "Satselect.h"
 #include <vector>
-#include <thread>
 
 int main() {
-	
+	using namespace Sgpsdp;
+
 	std::vector<NORAD_DATA> Satellites;
 	const std::string file = "TLE.txt";
 	Station st;
+
 	st.geo.Lat = 56.14717;
 	st.geo.Lon = 37.76275;
+	st.geo.Alt = 100;
 
-	st.geo.Alt = 100; 
 	st.lim.minAzm = 120.0;
 	st.lim.maxAzm = 200.0;
 	st.lim.minElv = 10.0;
 	st.lim.maxElv = 80.0;
-	st.lim.timeMinObserveSec = 50;
-	SetelliteSelect tmp(file, st);
-	tmp.dataPrepare();
-	Satellites = tmp.GetSatArray();
-	// for (int i = 0; i < 50; i++) {
-	// 	Satellites = tmp.GetSatArray();
-	// 	std::this_thread::sleep_for(std::chrono::seconds(5));
-	// }
-	tmp.showSat(Satellites);
+	st.lim.timeMinObserveSec = 20;
 
+	SetelliteSelect tmp(file, st);
+	Satellites = tmp.GetSatArray();
+	tmp.showSat(Satellites);
 	return 0;
 }
